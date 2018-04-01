d40232559e73a401f8eccd867149dc4d072d3725
Alex Kersten <alex@kersten.email>
2018-04-01 14:12:33 -0500

    remove leftover path

--- src/router/__init__.py
+++ src/router/__init__.py
@@ -5,7 +5,6 @@ from . import generic
 
 def init_routes(flask_app: Flask):
     flask_app.add_url_rule("/", "splash", methods=["GET"], view_func=generic.splash)
-    flask_app.add_url_rule("/app", "app", methods=["GET"], view_func=generic.app)
     flask_app.add_url_rule("/privacy", "privacy", methods=["GET"], view_func=generic.privacy)
     flask_app.add_url_rule("/license", "license", methods=["GET"], view_func=generic.license)
     flask_app.add_url_rule("/terms", "terms", methods=["GET"], view_func=generic.terms)
